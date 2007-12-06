73136b644c3928e22bb807209e2b47e41da44853
tim <tim@dockerz.net>
2007-12-06 21:40:28 +0000

    Use StateT to manage World state

--- Main.hs
+++ Main.hs
@@ -1,9 +1,10 @@
-import Graphics.Rendering.OpenGL
+import qualified Graphics.Rendering.OpenGL as GL
+import Graphics.Rendering.OpenGL( ($=) )
 import qualified Graphics.UI.SDL as SDL
-import Data.IORef
 import System.Time
 import System.Environment (getArgs)
 import Control.Monad
+import Control.Monad.State
 
 import Model
 import Render
@@ -14,13 +15,17 @@ data Config = Config {
     c_steps :: Int
     } deriving (Read,Show)
     
-data State = State {
+data World = World {
     now :: IO Time,
     model :: Model,
-    window_size :: Size,
-    sounds :: SoundState
+    window_size :: GL.Size,
+    sounds :: SoundState,
+    pending_event :: (Time,[Action])
 }
 
+type GState a = StateT World IO a
+
+main :: IO ()
 main = do
     args <- getArgs
     config <- (liftM read.readFile) (args !! 0)  :: IO Config
@@ -46,79 +51,83 @@ main = do
     let clock = tnow t0
     t <- clock
 
-    let (t',actions) = nextEvent t m
-    stv <- newIORef (State clock m (Size (fi width) (fi height)) sstate)
-    mainLoop (t,actions) stv
+    let ev = nextEvent t m
+    let w = World clock m (GL.Size (fi width) (fi height)) sstate ev
+    evalStateT mainLoop w
     endSound sstate
     SDL.quit
 
 setVideoMode width height =
     SDL.setVideoMode width height 32 [SDL.OpenGL,SDL.Resizable]
 
-mainLoop :: (Time,[Action]) -> IORef State -> IO ()
-mainLoop nexta stv = do
-    st <- readIORef stv
-    t <- now st
-    nexta' <- processActions t nexta st
+mainLoop :: GState ()
+mainLoop = do
+    st <- get
+    t <- liftIO $ now st
+    processActions t
 
-    SDL.delay 10
-    ev <- SDL.pollEvent
+    ev <- liftIO $ SDL.delay 10 >> SDL.pollEvent
     finished <- case ev of
       (SDL.VideoResize w h) -> do
-          modifyIORef stv (\st -> st{window_size=(Size (fi w) (fi h))})
-          setVideoMode w h
-          redraw st
-      (SDL.MouseButtonDown x y SDL.ButtonLeft) -> do
-          mouseClick stv x y
-          redraw st
+          modify (\st -> st{window_size=(GL.Size (fi w) (fi h))})
+          liftIO $ setVideoMode w h
+          redraw
+      (SDL.MouseButtonDown x y SDL.ButtonLeft) -> mouseClick x y >> redraw
       (SDL.MouseButtonDown _ _ SDL.ButtonWheelUp) -> modifySpeed (+5)
       (SDL.MouseButtonDown _ _ SDL.ButtonWheelDown) -> modifySpeed ((-)5)
-      SDL.VideoExpose -> redraw st
+      SDL.VideoExpose -> redraw
       (SDL.KeyDown  SDL.Keysym{SDL.symKey=SDL.SDLK_ESCAPE}) -> return True
       SDL.Quit -> return True
       _ -> return False
-    when (not finished) (mainLoop nexta' stv)
+    when (not finished) mainLoop
   where
-    redraw st = do
-      display st
-      SDL.glSwapBuffers
+    redraw :: GState Bool
+    redraw = do
+      st <- get
+      liftIO $ display st True
       return False
 
+    modifySpeed :: (Int->Int) -> GState Bool
     modifySpeed adj = do
-      updateModel stv (\m -> m{m_stepTime=adj (m_stepTime m)})
+      updateModel (\m -> m{m_stepTime=adj (m_stepTime m)})
       return True
-      
-processActions t nexta@(ta,actions) st = do
-    if (t >= ta) 
-      then do
-        mapM_ doAction actions
-        processActions t (nextEvent ta (model st)) st
-      else return nexta
-  where
-    doAction (Play c) = playSound (sounds st) c
-    doAction Repaint = display st
-    doAction FlipBuffer = SDL.glSwapBuffers
 
-mouseClick stv x y = modifyIORef stv uf
+processActions :: Time -> GState ()
+processActions t  = do
+    st <- get
+    let (ta,actions) = pending_event st
+    when (t >= ta) $ do
+        liftIO $ mapM_ (doAction st) actions
+        let ev = nextEvent ta (model st)
+        modify (\s -> s{pending_event=ev})
+        processActions t
   where
-    uf st = st{model=click (Vertex2 (fi x) (fi y)) g m}
+    doAction st (Play c) = playSound (sounds st) c
+    doAction st Repaint = display st False
+    doAction st FlipBuffer = SDL.glSwapBuffers
+
+mouseClick x y = modify uf
+  where
+    uf st = st{model=click (GL.Vertex2 (fi x) (fi y)) g m}
       where
         m = model st
         g = geometry (window_size st) m
 
-updateModel stv mf = modifyIORef stv (\st -> st{model=mf (model st)})
-    
-display st = do
-    t <- now st
-    let sz@(Size wWidth wHeight) = window_size st
-    viewport $= (Position 0 0, sz)
-    matrixMode $= Projection
-    loadIdentity
-    ortho2D 0 (fi wWidth) (fi wHeight) 0
-    clear [ColorBuffer]
-    preservingMatrix $ do
-        render sz t (model st)
-    flush
+updateModel mf = modify (\st -> st{model=mf (model st)})
+
+display :: World -> Bool -> IO ()
+display st swapBuffers = do
+        t <- now st
+        let sz@(GL.Size wWidth wHeight) = window_size st
+        GL.viewport $= (GL.Position 0 0, sz)
+        GL.matrixMode $= GL.Projection
+        GL.loadIdentity
+        GL.ortho2D 0 (fi wWidth) (fi wHeight) 0
+        GL.clear [GL.ColorBuffer]
+        GL.preservingMatrix $ do
+            render sz t (model st)
+        GL.flush
+        when swapBuffers SDL.glSwapBuffers
 
 tnow :: ClockTime -> IO Time
 tnow t0 = do
